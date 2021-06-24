7a009540d0bb99d5cfd3f91eaea6f8a85fa37187
Jered Wierzbicki <jeredw@gmail.com>
2021-06-24 07:29:02 -0700

    it's a computer!
    pas 80/88 ts 149/160 its 0/6
    a1  ***** ***.********   a2  ***.. **********..
    a3  ****. *********...   a4  ****. **..********
    a5  **... *********...   a6  ***.. **..*******.
    a7  **... **..******..   a8  *.... *...********
    a9  *.... *...********   a10 *.... **..********
    a11 *.... *...********   a12 ***** ************
    a13 **... *...********   a14 *.... *...********
    a15 ***.. ***.********   a16 **... **..********
    a17 ***** ************   a18 ****. ************
    a19 ***.. **..********   a20 ***.. ************
    0002> g
    0002>      00110
         00209
         00308
         00507
         00806
         01305
         02104
         03403
         05502
         08901
         09901

---
+++ fib.asm
@@ -0,0 +1,27 @@
+; compute fibonacci numbers
+.isa v4
+.org 100
+  mov 10, A   ; 10 numbers
+  swap A, B
+              ; D=Fn-2 (starting at 0)
+  mov 1, A    ; C=Fn-1
+  swap A, C
+
+loop
+  mov C, A    ; A=Fn-1
+  add D, A    ; A=Fn = Fn-1 + Fn-2
+  print AB
+  ; now A=Fn, C=Fn-1, D=Fn-2
+  ; want C=Fn, D=Fn-1
+  swap A, C   ; A=Fn-1, C=Fn
+  swap A, D   ; A=xx, C=Fn, D=Fn-1
+  mov B, A
+  dec A       ; dec loop counter
+  jz done     ; if 0, all done
+  swap B, A   ; update counter and continue
+  jmp loop
+
+done
+  mov 99, A
+  print AB
+  halt
