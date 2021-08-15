645f608e4bc5aa2e8e16785f44b6c43d4c89c8d7
Jered Wierzbicki <jeredw@gmail.com>
2021-08-15 13:18:53 -0700

    digit packed version of life w/copies
    around 165k add cycles per generation

--- asm/life.asm
+++ asm/life.asm
@@ -2,20 +2,23 @@
   .isa v4
   .org 100
 
-; The current and next state of an 8x8 grid are stored in words 0-63.
-; One digit of each word has the current state (0=dead or 1=alive), and the
-; other digit has the next state for the cell in word 8*y+x - which digit
-; is selected by the current generation# in E.  Initially, E=0 and the ones
-; digit has the current state.
+; The current state of an 8x8 grid is stored digit packed in words 0-31
+; (accs 0-6) and the next state in words 35-66 (accs 7-13).
+next_grid .equ 35
 
 ; Read initial state from cards
 init
   read            ; read F=position G=value
   swapall         ;
-  jil gen         ; illegal position (e.g. 99) signals done
-  jsr index       ; get B=address, A=value
-  mov A,[B]       ; set initial value for cell 
+  jil init_done   ; illegal position (e.g. 99) signals done
+  swap B,A        ; 
+  swap A,D        ; D=value
+  swap A,B        ; A=position
+  jsr set_cell
   jmp init
+
+init_done
+  jsr copy_grid
 
 ; Simulate one generation
 gen
@@ -38,7 +41,6 @@ pr_next_col
   mov A,C         ;
   jil pr_next_row ; if col==9, go to next row
 
-  jsr index       ; B=address for position
   jsr get_cell    ; A=cell value
   jz pr_next_col  ; if empty, don't print
   swap A,B        ; B=cell value
@@ -85,7 +87,6 @@ next_col
   dec A           ; A=(c-1, r-1)
   swapdig A       ; A=(r-1, c-1)
   jil count_n     ; skip if out-of-bounds
-  jsr index       ; B=address for (r-1,c-1)
   jsr get_cell    ; A=value
   jz count_n      ; 
   swap D,A        ;
@@ -98,7 +99,6 @@ count_n
   dec A           ; A=(c, r-1)
   swapdig A       ; A=(r-1, c)
   jil count_ne    ; skip if out-of-bounds
-  jsr index       ; B=address for (r-1,c)
   jsr get_cell    ; A=value
   jz count_ne     ; 
   swap D,A        ;
@@ -112,7 +112,6 @@ count_ne
   dec A           ; A=(c+1, r-1)
   swapdig A       ; A=(r-1, c+1)
   jil count_w     ; skip if out-of-bounds
-  jsr index       ; B=address for (r-1,c+1)
   jsr get_cell    ; A=value
   jz count_w      ; 
   swap D,A        ;
@@ -123,7 +122,6 @@ count_w
   mov C,A         ;
   dec A           ; A=(r, c-1)
   jil count_e     ; skip if out-of-bounds
-  jsr index       ; B=address for (r, c-1)
   jsr get_cell    ; A=value
   jz count_e      ; 
   swap D,A        ;
@@ -134,7 +132,6 @@ count_e
   mov C,A         ;
   inc A           ; A=(r, c+1)
   jil count_sw    ; skip if out-of-bounds
-  jsr index       ; B=address for (r, c+1)
   jsr get_cell    ; A=value
   jz count_sw     ; 
   swap D,A        ;
@@ -148,7 +145,6 @@ count_sw
   inc A           ; A=(c-1, r+1)
   swapdig A       ; A=(r+1, c-1)
   jil count_s     ; skip if out-of-bounds
-  jsr index       ; B=address for (r+1, c-1)
   jsr get_cell    ; A=value
   jz count_s      ; 
   swap D,A        ;
@@ -161,7 +157,6 @@ count_s
   inc A           ; A=(c, r+1)
   swapdig A       ; A=(r+1, c)
   jil count_se    ; skip if out-of-bounds
-  jsr index       ; B=address for (r+1, c)
   jsr get_cell    ; A=value
   jz count_se     ;
   swap D,A        ;
@@ -175,7 +170,6 @@ count_se
   inc A           ; A=(c+1, r+1)
   swapdig A       ; A=(r+1, c+1)
   jil rules       ; skip if out-of-bounds
-  jsr index       ; B=address for (r+1, c+1)
   jsr get_cell    ; A=value
   jz rules        ; 
   swap D,A        ;
@@ -185,7 +179,6 @@ count_se
   ; Apply rules for life
 rules
   mov C,A
-  jsr index       ; B=address for cur cel
   jsr get_cell    ; A=cur value
   jz cur_dead
 ;cur_alive        ; cell is currently alive
@@ -205,82 +198,107 @@ cur_dead          ; cell is currently dead
   jmp dead        ; else -> dead
 
 live
-  mov 10,A
+  mov 1,A<->D
+  mov C,A
   jsr set_cell    ; cell becomes alive
   jmp next_col
 dead
   clr A
+  swap A,D
+  mov C,A
   jsr set_cell    ; cell becomes dead
   jmp next_col
 
-  ; Toggle generation flag
+  ; Copy new grid over old grid
 gen_done
-toggle .table 1,0
-  swap E,A        ; generation (0 or 1) in A
-  add toggle,A
-  ftl A           ; lookup next generation in A
-  swap A,E        ; set next generation
+  jsr copy_grid
   jmp gen
 
 
-; Returns B=address for cell with index A
+; Returns A=current value of cell at position A
 ; offset table maps positions 11..88 to address
 ; NOTE relying on .table allocating this contiguously
 ; TODO support arithmetic on "offset" instead using a padding row
-offset  .table 0, 0, 0, 0, 0, 0, 0, 0, 0,0
-offset1 .table 0, 0, 1, 2, 3, 4, 5, 6, 7,0
-offset2 .table 0, 8, 9,10,11,12,13,14,15,0
-offset3 .table 0,16,17,18,19,20,21,22,23,0
-offset4 .table 0,24,25,26,27,28,29,30,31,0
-offset5 .table 0,32,33,34,35,36,37,38,39,0
-offset6 .table 0,40,41,42,43,44,45,46,47,0
-offset7 .table 0,48,49,50,51,52,53,54,55,0
-offset8 .table 0,56,57,58,59,60,61,62,63
-index
-  add offset,A
-  ftl A
-  swap A,B
-  ret
-
-; Returns A=current value of cell at address B
+offset  .table 0,  0,00,  0, 0,  0, 0,  0, 0,0
+offset1 .table 0,M00,00,M01,01,M02,02,M03,03,0
+offset2 .table 0,M04,04,M05,05,M06,06,M07,07,0
+offset3 .table 0,M08,08,M09,09,M10,10,M11,11,0
+offset4 .table 0,M12,12,M13,13,M14,14,M15,15,0
+offset5 .table 0,M16,16,M17,17,M18,18,M19,19,0
+offset6 .table 0,M20,20,M21,21,M22,22,M23,23,0
+offset7 .table 0,M24,24,M25,25,M26,26,M27,27,0
+offset8 .table 0,M28,28,M29,29,M30,30,M31,31,0
 get_cell
-  mov [B],A       ; read cell value
-  swap E,A        ; A=generation
-  jz get_cell_low ; if generation==0, low digit has cur state
-  swap A,E        ; reset E=generation
-  swapdig A       ; high digit has cur state 
-  jmp get_cell_out
-get_cell_low
-  swap A,E        ; reset E=generation
-get_cell_out
-  lodig A         ; get cur state
+  add offset,A    ; A=offset+position
+  ftl A           ; lookup address
+  jn get_cell_hi  ; if M use high digit
+  swap A,B
+  mov [B],A       ; get cell value
+  lodig A         ; extract low digit
+  ret
+get_cell_hi       ; 
+  swap A,B
+  mov [B],A       ; get cell value
+  swapdig A       ;
+  lodig A         ; get high digit
   ret
 
-; Sets the next value of cell at address B to A
-; (A=10 means the cell should turn on)
+; Sets the next value of cell at position A to D
 set_cell
-  swap A,D        ; D=next value (in high digit)
-
-  ;jsr get_cell    ; A=current value (in low digit)
-    mov [B],A       ; read cell value
-    swap E,A        ; A=generation
-    jz sgt_cell_low ; if generation==0, low digit has cur state
-    swap A,E        ; reset E=generation
-    swapdig A       ; high digit has cur state 
-    jmp sgt_cell_out
-sgt_cell_low
-    swap A,E        ; reset E=generation
-sgt_cell_out
-    lodig A         ; get cur state
-
-  add D,A         ; add in next value
-  swap E,A        ; A=generation
-  jz set_cell_hi  ; if generation==0, high digit has next state
-  swap A,E        ; reset E=generation
-  swapdig A       ; low digit has next state
-  jmp set_cell_out
+  add offset,A    ; A=offset+position
+  ftl A           ; lookup address
+  jn set_cell_hi  ; if M use high digit
+  ; Update low digit
+  add next_grid,A ; point into next_grid
+  swap A,B
+  mov [B],A       ; get cell value
+  swapdig A       ; hl -> lh
+  lodig A         ; 0h
+  swapdig A       ; 0h -> h0
+  add D,A         ; hD
+  mov A,[B]       ; update cell value
+  ret
+  ; Update hi digit
 set_cell_hi
-  swap E,A        ; reset E=generation
-set_cell_out
-  mov A,[B]       ; set current+next state
+  flipn           ; clear sign of offset
+  add next_grid,A ; point into next_grid
+  swap A,B
+  mov [B],A       ; get cell value
+  lodig A         ; 0l
+  swapdig A       ; l0
+  add D,A         ; lD
+  swapdig A       ; Dl
+  mov A,[B]       ; update cell value
+  ret
+
+; copy new grid state over old grid state
+copy_grid
+  mov 7,A
+  loadacc A 
+  clr A
+  storeacc A
+  mov 8,A
+  loadacc A 
+  mov 1,A
+  storeacc A
+  mov 9,A
+  loadacc A 
+  mov 2,A
+  storeacc A
+  mov 10,A
+  loadacc A 
+  mov 3,A
+  storeacc A
+  mov 11,A
+  loadacc A 
+  mov 4,A
+  storeacc A
+  mov 12,A
+  loadacc A 
+  mov 5,A
+  storeacc A
+  mov 13,A
+  loadacc A 
+  mov 6,A
+  storeacc A
   ret
