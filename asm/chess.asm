82b6ca56757655e6e67b899b9f5ed708259c1694
Jered Wierzbicki <jeredw@gmail.com>
2021-11-01 19:56:45 -0700

    cosmetic edits/comments

--- asm/chess.asm
+++ asm/chess.asm
@@ -8,13 +8,13 @@
   jmp far game       ; the game outer loop is in ft3
 done_squares         ; where movegen jumps when done
   jmp far no_more_moves
-  .include movegen.asm
-  .include get_square.asm
+  .include movegen.asm    ; 70 lines
+  .include get_square.asm ; 15 lines
 
   .org 200
-  .include move.asm
-  .include score.asm
-  ;.include checkcheck.asm
+  .include move.asm       ; 65 lines
+  .include score.asm      ; 11 lines
+  ;.include checkcheck.asm ; 41 lines
 
   .org 306
 game
@@ -38,75 +38,15 @@ start
   mov TOP1,A
   storeacc A        ; store TOP1
   ; init TOP0 to 0, setting all move-related state to zero
+  ; note this also sets [35]=[fromp] to 0, white to move
   clrall
   swapall
-  mov TOP0,A        ;
+  mov TOP0,A
   storeacc A
 
 search
+  ; call movegen for top of stack
   jmp far next_move
-
-; movegen jumps here when there are no more moves possible
-no_more_moves
-  ; if depth==1 then we have the best move at top of stack
-  mov depth,A<->B
-  mov [B],A
-  dec A
-  jz search_done    ; if depth==1, done
-
-  mov bestscore,A<->B
-  mov [B],A<->D     ; D=best score for this node
-  ; determine if the parent stack frame is for min or max
-  ; if parent is max, it will take max of its children, and so we should update
-  ; it only if our best score is better than its best, etc.
-  mov fromp,A<->B
-  mov [B],A         ; get current player (from player|piece)
-  swapdig A
-  lodig A           ; A=player
-  add oplayer,A
-  ftl A             ; A=1-player
-  jz .pmax          ; if white, score pmax
-;.pmin               ; else, score pmin
-  mov pbestscore,A<->B
-  mov [B],A         ; A=pbestscore
-  sub D,A           ; A=pbestscore - bestscore
-  flipn
-  jn .newbest       ; if bestscore < pbestscore, new best move
-  jmp .pop
-.pmax
-  mov pbestscore,A<->B
-  mov [B],A         ; A=pbestscore
-  sub D,A           ; A=pbestscore - bestscore
-  flipn
-  jn .pop           ; if bestscore < pbestscore, not a best move
-.newbest
-  swap D,A
-  mov A,[B]         ; [pbestscore] = current score
-  mov pfrom,A<->B
-  mov [B],A<->D     ; D=[pfrom]
-  mov pbestfrom,A<->B
-  swap D,A
-  mov A,[B]         ; [pbestfrom]=D
-  mov ptarget,A<->B
-  mov [B],A<->D     ; D=[ptarget]
-  mov pbestto,A<->B
-  swap D,A
-  mov A,[B]         ; [pbestto]=D
-
-.pop
-  ; no more moves at current depth.  parent stack frame has already
-  ; been updated with best move and score, so just pop
-  jsr pop
-  jsr undo_move
-  ; reset [fromp] for movegen
-  mov from,A<->B
-  mov [B],A<->D     ; D=[from] square
-  jsr get_square
-  swap A,D
-  mov fromp,A<->B
-  swap D,A
-  mov A,[B]         ; [fromp]
-  jmp search
 
 ; movegen jumps here when there is a move to try
 output_move
@@ -122,8 +62,10 @@ output_move
   ; the score for this node is the min or max of its children
   ; push a child search node
   jsr push
-  ; init bestscore according to whether this is a min/max frame
-  jsr flip_player
+  ; init child bestscore according to whether it is a min/max frame
+  jsr other_player  ; get other player
+  swapdig A         ; (swap into player field)
+  mov A,[B]         ; store new fromp (B=fromp)
   jz .max           ; if player is white then maximize score
 ;.min
   mov bestscore,A<->B
@@ -183,7 +125,63 @@ leaf
   jsr undo_move
   jmp search
 
-checkcheckret
+; movegen jumps here when there are no more moves possible
+no_more_moves
+  ; if depth==1 then we have the best move at top of stack
+  mov depth,A<->B
+  mov [B],A
+  dec A
+  jz search_done    ; if depth==1, done
+
+  ; done iterating over child moves, so bestscore is the final score
+  ; update parent's bestscore if this move is better than its current best
+  mov bestscore,A<->B
+  mov [B],A<->D     ; D=best score for this node
+  ; determine if the parent stack frame is for min or max
+  jsr other_player  ; get 1-player in A's low digit
+  jz .pmax          ; if white, score pmax
+;.pmin               ; else, score pmin
+  ; parent is min, update its best move if bestscore < pbestscore
+  mov pbestscore,A<->B
+  mov [B],A         ; A=pbestscore
+  sub D,A           ; A=pbestscore - bestscore
+  flipn
+  jn .newbest       ; if bestscore < pbestscore, new best move
+  jmp .pop
+.pmax
+  ; parent is max, update its best move if bestscore >= pbestscore
+  mov pbestscore,A<->B
+  mov [B],A         ; A=pbestscore
+  sub D,A           ; A=pbestscore - bestscore
+  flipn
+  jn .pop           ; if bestscore < pbestscore, not a best move
+.newbest
+  swap D,A
+  mov A,[B]         ; [pbestscore] = current score
+  mov pfrom,A<->B
+  mov [B],A<->D     ; D=[pfrom]
+  mov pbestfrom,A<->B
+  swap D,A
+  mov A,[B]         ; [pbestfrom]=D
+  mov ptarget,A<->B
+  mov [B],A<->D     ; D=[ptarget]
+  mov pbestto,A<->B
+  swap D,A
+  mov A,[B]         ; [pbestto]=D
+
+.pop
+  ; no more moves at current depth.  parent stack frame has already
+  ; been updated with best move and score, so just pop
+  jsr pop
+  jsr undo_move
+  ; reset [fromp] from board state for movegen
+  mov from,A<->B
+  mov [B],A<->D     ; D=[from] square
+  jsr get_square
+  swap A,D
+  mov fromp,A<->B
+  swap D,A
+  mov A,[B]         ; [fromp]
   jmp search
 
 search_done
@@ -199,15 +197,12 @@ search_done
 
   .include stack.asm
 
-; flip the current player for movegen
-; returns new player in A
-flip_player
+; returns the opposite player
+other_player
   mov fromp,A<->B
   mov [B],A         ; A=[fromp]
   swapdig A         ;
   lodig A           ; A=player field from current fromp
   add oplayer,A     ; 
   ftl A             ; A=1-current player
-  swapdig A         ; (swap into player field)
-  mov A,[B]         ; store new fromp
   ret
