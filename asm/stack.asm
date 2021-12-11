408585e31e857ca7718bc1a7dd19f2cc2ef35208
Jonathan Stray <jonathanstray@gmail.com>
2021-12-11 15:26:58 -0800

    Save another row in stack push

--- asm/stack.asm
+++ asm/stack.asm
@@ -7,9 +7,8 @@
 ;
 ; To make that copying more efficient using loadacc/storeacc, stack entries are
 ; stored with a stride of 10 words at offsets 36, 46, 56, and 66. Thus we can
-; copy quickly using loadacc/storeacc. Locations 35,35,55,65 are used for 
-; various game state and must be preserved.
-;
+; copy quickly using loadacc/storeacc. 
+; 
 ; a7  |xx 36 37 38 39
 ; a8   40 41 42 43 44|
 ; a9  |xx 46 47 48 49
@@ -18,15 +17,19 @@
 ; a12  60 61 62 63 64|
 ; a13 |xx 66 67 68 69
 ; a14  70 71 72 73 74|
+;
+; Locations 35,45,55,65 are not part of the stack and are handled as follows:
+;   35 - fromp  - don't need to preserve as we're not in movegen
+;   45 - wrook2 - must preserve
+;   55 - mscore
+;   65 - depth  - incremented/decremented
 
 ; Pop a move off the search stack, i.e. copy accumulators down
 pop
-  mov 35,A<->B
-  mov [B],A<->E   ; E=save [35]
   mov 45,A<->B
-  mov [B],A<->C   ; C=save [45]
+  mov [B],A<->C   ; C=save [45] = wrook2
   mov 55,A<->B
-  mov [B],A<->D   ; D=save [55]
+  mov [B],A<->D   ; D=save [55] = mscore
   mov 9,A
 .loop
   loadacc A       ; load acc A
@@ -39,16 +42,13 @@ pop
   flipn
   jmp .loop
 .out
-  ; fix [35], [45] and [55] which got clobbered
-  mov 35,A<->B
-  swap E,A
-  mov A,[B]       ; restore [35]
+  ; fix [45] and [55] which got clobbered
   mov 45,A<->B
   swap C,A
-  mov A,[B]       ; restore [45]
+  mov A,[B]       ; restore [45] = wrook2
   mov 55,A<->B
   swap D,A
-  mov A,[B]       ; restore [55]
+  mov A,[B]       ; restore [55] = mscore
   ; dec stack depth
   mov depth,A<->B
   mov [B],A
@@ -59,11 +59,11 @@ pop
 ; Push the search stack, i.e. copy accumulators up
 push
   mov 45,A<->B
-  mov [B],A<->C   ; C=save [45]
+  mov [B],A<->C   ; C=save [45] = wrook2
   mov 55,A<->B
-  mov [B],A<->D   ; D=save [55]
+  mov [B],A<->D   ; D=save [55] = mscore
   mov 65,A<->B
-  mov [B],A<->E   ; E=save [65]
+  mov [B],A<->E   ; E=save [65] = depth
   mov 12,A
 .loop
   loadacc A       ; load acc A
@@ -79,16 +79,13 @@ push
   ; fix [45], [55], and [65] which got clobbered
   mov 45,A<->B
   swap C,A
-  mov A,[B]       ; restore [45]
+  mov A,[B]       ; restore [45] =
   mov 55,A<->B
   swap D,A
   mov A,[B]       ; restore [55]
-  mov 65,A<->B
-  swap E,A
-  mov A,[B]       ; restore [65]
-  ; inc stack depth
+  ; inc stack depth, currently in E
   mov depth,A<->B
-  mov [B],A
+  mov E,A
   inc A
   mov A,[B]
   ret
